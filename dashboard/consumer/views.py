46dda4ee3ad680ce9619101edeab4d993611428f
Zorex Salvo <zsalvo@ayannah.com>
2017-06-07 14:13:27 +0800

    Improve the admin site

--- dashboard/consumer/views.py
+++ dashboard/consumer/views.py
@@ -10,23 +10,23 @@ import datetime
 class MessageView(generic.ListView):
     template_name='home.html'
     context_object_name = 'message_list'
-    query_results = Message.objects.all()
+    query_results = Message.objects.all().order_by('-timestamp')
     
     def get_queryset(self):
         start_date = self.request.GET.get("datetimepicker1")
         end_date = self.request.GET.get("datetimepicker2")
         
         if start_date and end_date:
-            s_date = datetime.datetime.strptime(start_date,'%Y/%m/%d %H:%M')
-            e_date = datetime.datetime.strptime(end_date,'%Y/%m/%d %H:%M')
+            start = datetime.datetime.strptime(start_date,'%Y/%m/%d %H:%M')
+            end = datetime.datetime.strptime(end_date,'%Y/%m/%d %H:%M')
 
-            if s_date <= e_date:
+            if start <= end:
                 return Message.objects.filter(
-                    timestamp__range = (s_date, e_date)
+                    timestamp__range = (start, end)
                 ).order_by("-timestamp")
             else:
                 return Message.objects.filter(
-                    timestamp__range = (e_date, s_date)
+                    timestamp__range = (end, start)
                 ).order_by("-timestamp")
         else:
             return Message.objects.filter(
